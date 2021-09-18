26e4e5e6598dd2e5c76cabedc0a66865e3d4d3fb
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-09-18 03:30:21 -0300

    - created login, animals and default list routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalListComponent } from './components/pages/animal/animal-list/animal-list.component';
+import { LoginComponent } from './components/pages/login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'animals/list', component: AnimalListComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
